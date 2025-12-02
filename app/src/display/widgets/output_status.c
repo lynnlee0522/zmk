74c04635d738753d197b84545133047253af05ce
lynnlee0522 <lilin@arcblock.io>
2025-12-02 22:17:33 +0800

    fix to st7789

--- app/src/display/widgets/output_status.c
+++ app/src/display/widgets/output_status.c
@@ -41,35 +41,44 @@ static struct output_status_state get_state(const zmk_event_t *_eh) {
 
 static void set_status_symbol(lv_obj_t *label, struct output_status_state state) {
     // 设置label使用自定义符号字体
+    // LV_FONT_DECLARE(lv_symbol_lock);
+    // LV_FONT_DECLARE(lv_symbol_unlock);
 
+    lv_label_set_recolor(label, true);
     char text[40] = {};
+
     switch (state.selected_endpoint.transport) {
     case ZMK_TRANSPORT_USB:
-        snprintf(text, sizeof(text), LV_SYMBOL_USB);
+        snprintf(text, sizeof(text), "#ffffff " LV_SYMBOL_USB "#");
         break;
     case ZMK_TRANSPORT_BLE:
         if (state.active_profile_bonded) {
             if (state.active_profile_connected) {
                 // 已配对且已连接
                 snprintf(text, sizeof(text),
-                         LV_SYMBOL_BLUETOOTH_1 " %i " LV_SYMBOL_LOCK " " LV_SYMBOL_OK,
+                         "#ffffff " LV_SYMBOL_BLUETOOTH " %i " LV_SYMBOL_LOCK " #"
+                         "#00ff00 " LV_SYMBOL_OK " #",
                          state.selected_endpoint.ble.profile_index + 1);
             } else {
                 // 已配对但未连接
                 snprintf(text, sizeof(text),
-                         LV_SYMBOL_BLUETOOTH_1 " %i " LV_SYMBOL_LOCK " " LV_SYMBOL_CLOSE,
+                         "#ffffff " LV_SYMBOL_BLUETOOTH " %i " LV_SYMBOL_LOCK " #"
+                         "#ff0000 " LV_SYMBOL_CLOSE " #",
                          state.selected_endpoint.ble.profile_index + 1);
             }
         } else {
             // 未配对
-            snprintf(text, sizeof(text), LV_SYMBOL_BLUETOOTH_1 " %i " LV_SYMBOL_UNLOCK,
+            snprintf(text, sizeof(text),
+                     "#ffffff " LV_SYMBOL_BLUETOOTH " %i " LV_SYMBOL_UNLOCK " #",
                      state.selected_endpoint.ble.profile_index + 1);
         }
         break;
     }
 
     // 设置text的颜色为白色，字体大小为22
-    // lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN);
+    // lv_obj_set_style_text_font(label, &lv_font_montserrat_20, LV_PART_MAIN);
+    lv_obj_set_style_text_font(label, &lv_custom_symbol, LV_PART_MAIN);
+    lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN);
 
     lv_label_set_text(label, text);
 }
